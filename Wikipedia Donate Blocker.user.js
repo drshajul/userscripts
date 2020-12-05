126877998bb852fb356b64ddbc6b826a31cf7c8e
Dr Shajul <drshajul@gmail.com>
2020-12-05 22:29:39 +0530

    Added wikipedia india donation blocker

---
+++ Wikipedia Donate Blocker.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Wikipedia Donate Blocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Block the donation prompts!
+// @author       You
+// @match        https://*.wikipedia.org/wiki/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelectorAll('div.frb').forEach(
+        function myFunction(item, index, arr) {item.style.display = "none";}
+    )
+})();
