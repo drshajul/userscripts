39aa295cbc8fea13629419bc5cce3c7413f092ed
Dr S George <drshajul@gmail.com>
2023-06-09 15:03:48 +0530

    Add ReloadAMP License

--- ReloadAMP.user.js
+++ ReloadAMP.user.js
@@ -7,6 +7,7 @@
 // @include      https://news.google.*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        unsafeWindow
+// @license      MIT
 // ==/UserScript==
 
 (function() {
